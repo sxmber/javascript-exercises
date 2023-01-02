659dd2d6f1958b3436e0f15086122124342fcda7
sxmber <williamsmd002@gmail.com>
2023-01-02 15:51:52 -0700

    started 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+    let finalSum = 0;
+    for (i = first; i <= last; i++) {
+        finalSum += i;
+    }
+    console.log(finalSum);
+    return finalSum;
 };
-
+sumAll(1, 5);
 // Do not edit below this line
 module.exports = sumAll;
