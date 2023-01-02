ceaaa6b1947669d35f0cb97414ba2d59f73bbb9f
sxmber <williamsmd002@gmail.com>
2023-01-02 09:21:14 -0700

    finished 03 practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let myArray = string.split("");
+    let result = "";
+
+    for (i = myArray.length - 1; i >= 0; i--) {
+        result += myArray[i];
+        console.log(result);
+        // resultArray.push(myArray[i]);
+    }
+    return result;
 
 };
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
