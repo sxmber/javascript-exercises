81b6fa2a847040883fa1e2f5303c470db75cbf46
sxmber <williamsmd002@gmail.com>
2023-01-03 10:04:43 -0700

    finished 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,25 @@
 const sumAll = function (first, last) {
     let finalSum = 0;
-    for (i = first; i <= last; i++) {
-        finalSum += i;
-    }
-    console.log(finalSum);
-    return finalSum;
+    if (first >= 0 && last >= 0 && Number.isInteger(first) == true && Number.isInteger(last) == true) {
+        if (first < last) {
+            for (i = first; i <= last; i++) {
+                finalSum += i;
+            }
+            console.log(finalSum);
+            return finalSum;
+        } else {
+            for (i = last; i <= first; i++) {
+                finalSum += i;
+
+            }
+            console.log(finalSum);
+            return (finalSum);
+        }
+
+    } return "ERROR";
+
+
 };
-sumAll(1, 5);
+sumAll(5, 1);
 // Do not edit below this line
 module.exports = sumAll;
