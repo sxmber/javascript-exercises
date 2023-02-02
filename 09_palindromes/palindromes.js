22d2562d7ccfff57e6ccf923289b538d3a2ec5cd
sxmber <williamsmd002@gmail.com>
2023-02-02 09:17:17 -0700

    finished calculator and palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (a) {
+    //create new array that makes all characters lowercase, splits each char into into own index and reverses their order
+    let reversedArray = a.toLowerCase().split("").reverse();
+    let originalArray = a.toLowerCase().split("");
+    console.log(reversedArray);
+    //filter function that removes spaces and puncutation
+    //NOTE: IF REMOVING ITEMS FROM ARRAYS THROUGH LOOPS, ITERATE BACKWARDS TO AVOID SKIPPING INDEXES
+    function clean(array) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (array[i] === ' ' || array[i] === ',' || array[i] === "." || array[i] === "!") {
+                array.splice(i, 1);
+            }
+        }
 
+    }
+    clean(reversedArray);
+    clean(originalArray);
+    //converts the arrays to strings
+    reversedArray = reversedArray.join('')
+    originalArray = originalArray.join('')
+    return reversedArray === originalArray ? true : false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
