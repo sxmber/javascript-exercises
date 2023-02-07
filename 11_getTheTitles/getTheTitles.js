176f1fe3bc2c322557d81ac0f4bb97e89d34090f
sxmber <williamsmd002@gmail.com>
2023-02-06 19:44:36 -0700

    finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    let newArray = array.map((title) => {
+        return title.title;
+    })
+    return newArray;
 };
 
 // Do not edit below this line
